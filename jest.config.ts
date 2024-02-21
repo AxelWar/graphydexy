c46b43deeb23e96ce6c47ff08448e1d766c7b09a
Axel <axelwarneke@gmail.com>
2024-02-20 21:07:07 -0300

    develop/jest

--- jest.config.ts
+++ jest.config.ts
@@ -161,9 +161,7 @@ const config: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/src/test.ts'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
